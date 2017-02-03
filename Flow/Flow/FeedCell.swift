c1eca5ea3a78c5e85a04e3fa6b0088d94d3866a4
David Chu <davidchuyaya@gmail.com>
2017-02-03 17:04:34 -0500

    Redesign feed, removed search, feed options, changed pictures

--- Flow/Flow/FeedCell.swift
+++ Flow/Flow/FeedCell.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class FeedCell:UITableViewCell
 {
+    @IBOutlet weak var container: UIView!
     @IBOutlet weak private var eventImage: UIImageView!
     @IBOutlet weak private var eventTitle: UILabel!
     @IBOutlet weak private var eventCaption: UILabel!
     @IBOutlet weak private var eventDate: UILabel!
     
+    override func awakeFromNib()
+    {
+        //roundCorners()
+    }
+    private func roundCorners()
+    {
+        let maskPath = UIBezierPath(roundedRect: container.bounds, byRoundingCorners: .allCorners, cornerRadii: CGSize(width: 10.0, height: 10.0))
+        let maskLayer = CAShapeLayer(layer: maskPath)
+        maskLayer.frame = container.bounds
+        maskLayer.path = maskPath.cgPath
+        container.layer.mask = maskLayer
+    }
     func configure(title:String, caption:String, date:String)
     {
         eventTitle.text = title
--- Flow/Flow/FeedVC.swift
+++ Flow/Flow/FeedVC.swift
@@ -14,18 +14,12 @@ class FeedVC:UITableViewController, UISearchBarDelegate
     let data = [["Hello", "World", "2/12"],
                 ["Chance the Rapper: All Night Concert", "3904 Jabari Shoal Suite 087, Happy City, New York", "12/31"],
                 ["Dinner", "Appel", "Today"]]
-    let feedOptions = ["Trending", "Nightlife", "Health & Fitness", "Concerts & Shows", "Art & Culture", "Philanthropy", "Food", "Education"]
-    var titleButton: UIButton!
-    var feedOptionButtons: [UIButton]!
-    var blurView: UIVisualEffectView!
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
         setRowHeight()
-        initNavBarOverlay()
-        initTableBlur()
-        initFeedOptionButtons()
+        setTitleFont()
     }
     
     /**
@@ -37,115 +31,11 @@ class FeedVC:UITableViewController, UISearchBarDelegate
         tableView.rowHeight = totalHeight / 3
     }
     /**
-     Turns the navigation bar into a button.
+     Sets the font of the navigation bar title
      */
-    private func initNavBarOverlay()
+    private func setTitleFont()
     {
-        titleButton = UIButton(type: .system)
-        setNavBarTitle(feedOptions[0])
-        titleButton.setTitleColor(UIColor.black, for: .normal)
-        titleButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
-        titleButton.addTarget(self, action: #selector(onNavBarClick), for: .touchUpInside)
-        navigationItem.titleView = titleButton
-    }
-    func onNavBarClick()
-    {
-        animateFeedOptionButtons()
-    }
-    /**
-     Takes a desired navigation bar title and appends the downward arrow.
-     */
-    private func setNavBarTitle(_ title:String)
-    {
-        titleButton.setTitle("\(title) ▼", for: .normal)
-        titleButton.sizeToFit()
-    }
-    /**
-     Initializes the view that will put a "frosted glass" effect over the table when the user wants to select a different feed option.
-     */
-    private func initTableBlur()
-    {
-        blurView = UIVisualEffectView()
-        blurView.frame = view.frame
-        view.addSubview(blurView)
-    }
-    /**
-     Creates all the feed option buttons, setting their constraints and text. The buttons are then turned invisible.
-     - important: Since the buttons should appear on the top of everything else, they should be initialized last, specifically after the table blur
-     */
-    private func initFeedOptionButtons()
-    {
-        feedOptionButtons = [UIButton]()
-        for i in 0..<feedOptions.count
-        {
-            let button = UIButton(type: .system)
-            button.translatesAutoresizingMaskIntoConstraints = false
-            button.setTitle(feedOptions[i], for: .normal)
-            button.addTarget(self, action: #selector(onFeedOptionButtonClick), for: .touchUpInside)
-            feedOptionButtons.append(button)
-            blurView.contentView.addSubview(button)
-            
-            if (i == 0)
-            {
-                view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:[view]-16-[button]", options: .alignAllCenterX, metrics: nil, views: ["view":view, "button":button]))
-            }
-            else
-            {
-                view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:[previousButton]-16-[button]", options: .alignAllCenterX, metrics: nil, views: ["previousButton":feedOptionButtons[i - 1], "button":button]))
-            }
-            button.alpha = 0.0
-        }
-    }
-    /**
-     Animate feed option buttons either appearing or fading. If appearing, start appearing from the top. If fading, start fading from the bottom. The buttons will fade if they are currently showing; they'll appear if they're currently faded.
-     
-     Fading will not begin unless all the buttons are either present or not present (an animation is NOT occurring).
-     
-     The table view will also be disabled/enabled, with a "frosted glass" effect over it.
-     */
-    private func animateFeedOptionButtons()
-    {
-        let lastButtonAlpha = feedOptionButtons[feedOptionButtons.count - 1].alpha
-        let firstButtonAlpha = feedOptionButtons[0].alpha
-        /*
-         Cases:
-         1. We have finished making all buttons appear. The last button to appear is the last button, so if the last button has alpha = 1.0, then all buttons have appeared.
-         2. We have finished making all buttons disappear. The last button to disappear is the first button, so if the first button has alpha = 0.0, then all buttons have disappeared.
-         */
-        guard lastButtonAlpha == 1.0 || firstButtonAlpha == 0.0 else {
-            return
-        }
-        
-        let fade = lastButtonAlpha == 1.0
-        if (fade)
-        {
-            for i in 1...feedOptionButtons.count
-            {
-                UIView.animate(withDuration: 0.05, delay: Double(i) * 0.05, options: .curveLinear, animations: {self.feedOptionButtons[self.feedOptionButtons.count - i].alpha = 0.0}, completion: nil)
-            }
-            UIView.animate(withDuration: Double(feedOptionButtons.count) * 0.05, animations: {self.blurView.effect = nil})
-            tableView.isScrollEnabled = true
-        }
-        else
-        {
-            for i in 0..<feedOptionButtons.count
-            {
-                UIView.animate(withDuration: 0.05, delay: Double(i) * 0.05, options: .curveLinear, animations: {self.feedOptionButtons[i].alpha = 1.0}, completion: nil)
-            }
-            UIView.animate(withDuration: Double(feedOptionButtons.count) * 0.05, animations: {self.blurView.effect = UIBlurEffect(style: .light)})
-            tableView.isScrollEnabled = false
-        }
-    }
-    /**
-     When a feed option button is clicked, change the current title of the navigation bar and hide all the feed options.
-     */
-    func onFeedOptionButtonClick(_ button:UIButton)
-    {
-        guard let title = button.currentTitle else {
-            return
-        }
-        animateFeedOptionButtons()
-        setNavBarTitle(title)
+        navigationController?.navigationBar.titleTextAttributes = [NSFontAttributeName:UIFont.init(name: "Copperplate", size: 32)!]
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
@@ -158,10 +48,5 @@ class FeedVC:UITableViewController, UISearchBarDelegate
         let dataForCell = data[indexPath.row]
         cell.configure(title: dataForCell[0], caption: dataForCell[1], date: dataForCell[2])
         return cell
-    }
-    func searchBarShouldBeginEditing(_ searchBar: UISearchBar) -> Bool
-    {
-        performSegue(withIdentifier: "showSearchView", sender: self)
-        return true
     }
 }
